791eb492b552a7ccdad1d2f56b4de305cbec948f
stop666two <stop666bilibili@gmail.com>
2026-07-21 21:29:22 +0800

    chore: remove license system, enable string_cipher mixed by default

--- output/main.py
+++ output/main.py
@@ -1,38 +1,36 @@
 import sys, os, json, hashlib, zlib
 _R = getattr(sys, '_MEIPASS', None) or os.path.dirname(os.path.abspath(__file__))
 
-def _xof(_94d19d3e73, _0xde05f2d8):
-    _v9246_3904, _t28bca78a9e = (bytearray(), 0)
-    while len(_v9246_3904) < _94d19d3e73:
-# DEBUG: remove before production
-# NOTE: deprecated in 3.12
 # monkey patch for compatibility
-# pylint: disable=unused-variable
-# XXX: known issue
+# TODO: implement error handling
+# See bugs.python.org/issue12345
 
-        _v9246_3904.extend(hashlib.sha256(_0xde05f2d8 + _t28bca78a9e.to_bytes(4, 'big')).digest())
-        _t28bca78a9e += 1
-    return bytes(_v9246_3904[:_94d19d3e73])
+def _xof(_30ef9781cd, _0x2db4eccb):
+    _v2112_3487, _taa848bb643 = (bytearray(), 0)
+    while len(_v2112_3487) < _30ef9781cd:
+        _v2112_3487.extend(hashlib.sha256(_0x2db4eccb + _taa848bb643.to_bytes(4, 'big')).digest())
+        _taa848bb643 += 1
+    return bytes(_v2112_3487[:_30ef9781cd])
 
-def _boot(_fa4c6fa2d241):
+def _boot(_6b044e4fe483):
     from cryptography.hazmat.primitives.ciphers.aead import AESGCM, ChaCha20Poly1305
-    _73343_2d99f1 = json.loads(open(os.path.join(_R, '_runtime', 'loader.pye'), 'rb').read().decode())
-    _12041 = _fa4c6fa2d241
-    for _y12f1b522 in ['k1', 'k2', 'k3', 'k4', 'k5']:
-        if _y12f1b522 in _73343_2d99f1:
-            _2b724bd3031647 = bytes.fromhex(_73343_2d99f1[_y12f1b522])
-            _d810 = hashlib.sha256(_2b724bd3031647).digest()[:4].hex()
-            if _d810 == _73343_2d99f1.get('f1', '')[:8] or _d810 == _73343_2d99f1.get('f2', '')[:8] or _d810 == _73343_2d99f1.get('f3', '')[:8]:
-                _12041 = _2b724bd3031647
+    _70833_b1da5f = json.loads(open(os.path.join(_R, '_runtime', 'loader.pye'), 'rb').read().decode())
+    _5500 = _6b044e4fe483
+    for _x36ce8a9a in ['k1', 'k2', 'k3', 'k4', 'k5']:
+        if _x36ce8a9a in _70833_b1da5f:
+            _fd7d1fa3827b59 = bytes.fromhex(_70833_b1da5f[_x36ce8a9a])
+            _b3168 = hashlib.sha256(_fd7d1fa3827b59).digest()[:4].hex()
+            if _b3168 == _70833_b1da5f.get('f1', '')[:8] or _b3168 == _70833_b1da5f.get('f2', '')[:8] or _b3168 == _70833_b1da5f.get('f3', '')[:8]:
+                _5500 = _fd7d1fa3827b59
                 break
-    __3ad81808b4eb = bytes.fromhex(_73343_2d99f1['d'])
-    _abfbdd9c7eaf39 = AESGCM(_12041).decrypt(__3ad81808b4eb[:12], __3ad81808b4eb[12:], b'')
-    _abfbdd9c7eaf39 = bytes((_q18801c_32 ^ _802c04bd194 for _q18801c_32, _802c04bd194 in zip(_abfbdd9c7eaf39, _xof(len(_abfbdd9c7eaf39), _12041))))
+    __46d4e1b0b37d = bytes.fromhex(_70833_b1da5f['d'])
+    _4c870fb15b30a5 = AESGCM(_5500).decrypt(__46d4e1b0b37d[:12], __46d4e1b0b37d[12:], b'')
+    _4c870fb15b30a5 = bytes((_q5a3f88_24 ^ _6a8530e9580 for _q5a3f88_24, _6a8530e9580 in zip(_4c870fb15b30a5, _xof(len(_4c870fb15b30a5), _5500))))
     try:
-        _abfbdd9c7eaf39 = ChaCha20Poly1305(_12041).decrypt(_abfbdd9c7eaf39[:12], _abfbdd9c7eaf39[12:], b'')
+        _4c870fb15b30a5 = ChaCha20Poly1305(_5500).decrypt(_4c870fb15b30a5[:12], _4c870fb15b30a5[12:], b'')
     except Exception:
         pass
-    return zlib.decompress(_abfbdd9c7eaf39).decode()
-_ld = compile(_boot(bytes.fromhex('aac895763859dddfe65ddcc9ae3c778398bf39c5f0aaff9e2894678291f21f17')), '', 'exec')
+    return zlib.decompress(_4c870fb15b30a5).decode()
+_ld = compile(_boot(bytes.fromhex('32d4d9131f5c8be2e5ae6eff09c34d198c2381a0d22217af25c3331158ed822b')), '', 'exec')
 exec(_ld)
 run('main', _R)
